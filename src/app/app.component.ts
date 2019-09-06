eb999d06519f5e1dece3e5a23a94c5f4e6cc7d81
Unknown <robinelzeard@gmail.com>
2019-09-06 16:45:20 +0200

    add firebase and leaflet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import {FirebaseService} from './services/firebase.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+  
   title = 'CartoLeafletAngular';
+  constructor(
+    private firebase:FirebaseService
+  ){
+    
+  };  
 }
---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+  constructor() { 
+    const firebaseConf ={
+      apiKey: '${FIREBASE_KEY}', //FIREBASE_KEY is stored in netlify env var
+      authDomain: "projetleaflet.firebaseapp.com",
+      databaseURL: "https://projetleaflet.firebaseio.com",
+      projectId: "projetleaflet",
+      storageBucket: "",
+      messagingSenderId: "338184040216",
+      appId: "1:338184040216:web:34d3bdac5f23773b"    
+  }
+    firebase.initializeApp(firebaseConf)
+    console.log("firebase lancé")
+  }
+}
